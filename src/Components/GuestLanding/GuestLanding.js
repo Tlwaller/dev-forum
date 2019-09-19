e7ef517e690a8b04283e87ea2cef11e33a47c4c7
Trey Waller <Trey@Treys-MacBook-Pro.local>
2019-09-19 14:16:50 -0500

    barebones components

---
+++ src/Components/GuestLanding/GuestLanding.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function GuestLanding() {
+    return (
+        <div>
+            <h1>Guest Landing</h1>
+        </div>
+    )
+}
---
+++ src/Components/UserLanding/UserLanding.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function UserLanding() {
+    return (
+        <div>
+            <h1>User Landing</h1>
+        </div>
+    )
+}
